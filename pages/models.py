163920d60e5313207210783aaae7cf7639ae9ab1
Adam Hoddinott <adam@messymind.net>
2013-03-05 16:51:10 +1100

    Use object manager directly

--- pages/models.py
+++ pages/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-
 from entropy.base import SlugMixin, TitleMixin, TextMixin
-
 
 try:
     # Only import from platforms if it is a dependancy
     from platforms import models as platforms_models
     # Use platform mixin if platforms is found as a dependancy
-    PlatformObjectManagerMixin = platforms_models.PlatformObjectManagerMixin
+    ObjectManager = platforms_models.PlatformObjectManager
 except ImportError:
-    PlatformObjectManagerMixin = object
+    ObjectManager = models.Manager
 
 
-class Page(SlugMixin, TitleMixin, TextMixin, PlatformObjectManagerMixin):
-    pass
+class Page(SlugMixin, TitleMixin, TextMixin):
+
+    objects = ObjectManager()
