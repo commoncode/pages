351b054e1dc5cf5b4813742d1f80da24c4a95d3c
Adam Hoddinott <adam@messymind.net>
2013-03-07 11:28:02 +1100

    Add migrations

---
+++ pages/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Page'
+        db.create_table(u'pages_page', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('text', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('short_title', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=255)),
+        ))
+        db.send_create_signal(u'pages', ['Page'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Page'
+        db.delete_table(u'pages_page')
+
+
+    models = {
+        u'pages.page': {
+            'Meta': {'object_name': 'Page'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'short_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['pages']
---
+++ pages/migrations/0002_auto__add_field_page_enabled.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Page.enabled'
+        db.add_column(u'pages_page', 'enabled',
+                      self.gf('django.db.models.fields.BooleanField')(default=False, db_index=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Page.enabled'
+        db.delete_column(u'pages_page', 'enabled')
+
+
+    models = {
+        u'pages.page': {
+            'Meta': {'object_name': 'Page'},
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'short_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['pages']
