360207230e658f9362e464f34370fae30e266bef
Adam Hoddinott <adam@messymind.net>
2013-04-05 10:29:37 +1100

    Bumping version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup( name='pages',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Pages, based on entropy.',
     author = 'Daryl Antony',
     author_email = 'daryl@commoncode.com.au',
