21b76e4d3ca6db847f0476af02e530a52b6ca4ea
Y3K <dboy.y3k@gmail.com>
2014-06-01 19:41:02 -0500

    Remove Platforms manager

--- pages/models.py
+++ pages/models.py
@@ -6,18 +6,6 @@ from entropy.base import (SlugMixin, TitleMixin, TextMixin, ModifiedMixin,
     CreatedMixin, PublishingStatusMixin, AttributeMixin)
 from entropy.fields import EnabledField
 
-try:
-    # Only import from platforms if it is a dependancy
-    from platforms import settings as platforms_settings
-    if platforms_settings.USE_PLATFORMS:
-        from platforms import models as platforms_models
-        # Use platform mixin if platforms is found as a dependancy
-        ObjectManager = platforms_models.PlatformObjectManager
-    else:
-        raise ImportError
-except ImportError:
-    ObjectManager = models.Manager
-
 
 class Page(CQRSModel, SlugMixin, TitleMixin, TextMixin, ModifiedMixin,
     CreatedMixin, PublishingStatusMixin, AttributeMixin):
@@ -25,8 +13,6 @@ class Page(CQRSModel, SlugMixin, TitleMixin, TextMixin, ModifiedMixin,
     enabled = EnabledField()
     featured = models.BooleanField(default=False)
     published_date = models.DateField(null=True)
-
-    objects = ObjectManager()
 
     def get_absolute_url(self):
         """Returns the absolute url for a single page instance
